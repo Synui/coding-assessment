8acf6305c1512c4709716013b1cba5704bfbd2fb
Synui <jonathon.schoenholz@yahoo.com>
2021-11-27 02:08:42 -0500

    Partial questions

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+//questions for assessment
+const questions = [
+    {
+        question: "What is the correct syntax to close the <title> element?",
+        choices: ["<?title>", "<!title>", "</title>", "<title>"],
+        answer: "</title>"
+    },
+    {
+        question: "What are the git commands for pushing files to github to MAIN?",
+        choices: ["git pull origin develop", "git push origin main", "git push origin develop", "git merge main"],
+        answer: "git push origin main"
+    },
+    {
+        question: "What is the purpose of the alt attribute for images?",
+        choices: ["To make the image load faster", "To make it easier to style the image with CSS", "To prevent search engines from indexing the image", "To provide context for the image"],
+        answer: "To provide context for the image"
+    },
+    {
+        question: 
+        choices: 
+        answer:
+    },
+    {
+        question: 
+        choices: 
+        answer:
+    },
+    {
+        question: 
+        choices: 
+        answer:
+    },
+    {
+        question: 
+        choices: 
+        answer:
+    },
+    {
+        question: 
+        choices: 
+        answer:
+    },
+]
